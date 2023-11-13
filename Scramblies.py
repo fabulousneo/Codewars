e84432b9d18afbfa4d988ed3aa8363f3b8c760b4
Vladislav <Ledovskikh.vladislav@yandex.ru>
2023-11-13 22:15:58 +0300

    Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.

---
+++ Scramblies.py
@@ -0,0 +1,15 @@
+def scramble(s1, s2):
+    for i in range(len(s2)):
+        if s2[i] in s1:
+           s1 = s1.replace(s2[i], '', 1)
+           continue
+        else:
+           return False
+    return True
+
+def large_test():
+        s1 = "rkqodlw"
+        s2 = "world"
+        print(scramble(s1, s2))
+
+large_test()
