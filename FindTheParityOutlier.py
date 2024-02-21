b752957e6cc35e31faffe62b2cabba640ecbcc3c
Владислав Ледовских <ledovskikh.vladislav@yandex.ru>
2024-02-21 15:16:58 +0300

    1

--- FindTheParityOutlier.py
+++ FindTheParityOutlier.py
@@ -1,2 +1,20 @@
+import random
+
 def find_outlier(integers):
-    return None
+    one = random.randint(0, len(integers) - 1)
+    two = random.randint(0, len(integers) - 1)
+    while True:
+        if integers[one] % 2 == 0 & integers[two] % 2 == 0:
+            for i in integers:
+                if i % 2 != 0:
+                    print(i)
+                    break
+        else:
+            for i in integers:
+                if i % 2 == 0:
+                    print(i)
+                    break
+            
+
+
+print(find_outlier([2,6,8,200,700,1,84,10,4]))
