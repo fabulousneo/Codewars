4fcd3f5899cc3eb0aad9046aea78020e7c34d85d
Владислав Ледовских <ledovskikh.vladislav@yandex.ru>
2024-02-22 20:50:14 +0300

    You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.

--- FindTheParityOutlier.py
+++ FindTheParityOutlier.py
@@ -1,20 +1,11 @@
-import random
-
 def find_outlier(integers):
-    one = random.randint(0, len(integers) - 1)
-    two = random.randint(0, len(integers) - 1)
-    while True:
-        if integers[one] % 2 == 0 & integers[two] % 2 == 0:
-            for i in integers:
-                if i % 2 != 0:
-                    print(i)
-                    break
-        else:
-            for i in integers:
-                if i % 2 == 0:
-                    print(i)
-                    break
-            
-
-
-print(find_outlier([2,6,8,200,700,1,84,10,4]))
+    even_values = [i for i in integers if i % 2 == 0]
+    
+    if len(even_values) > 1:
+        for i in integers:
+            if i % 2 != 0:
+                return i
+    else:
+        return even_values[0]
+        
+print(find_outlier([2,6,8,10,3]))
