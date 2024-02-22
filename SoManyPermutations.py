85cf1bf95c85fc1defe1a740ab7bf62a1db48cc7
Владислав Ледовских <ledovskikh.vladislav@yandex.ru>
2024-02-22 21:33:59 +0300

    1

---
+++ SoManyPermutations.py
@@ -0,0 +1,11 @@
+def permutations(s):
+    len_word = len(s)
+    sym = [i for i in s]
+    result = []
+    for i in range(20):
+        sym[i]
+    return sym
+
+print(permutations('aaaaaaab'))
+
+# Пока не решено
